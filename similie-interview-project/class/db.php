3269245c42b1964463329c41ba8af4e969e406ac
glotoeric1 <glotoeric@gmail.com>
2020-02-01 00:03:34 +0200

    Update db.php

--- similie-interview-project/class/db.php
+++ similie-interview-project/class/db.php
@@ -2,8 +2,8 @@
     // Database configuration
     $dbHost     = "localhost";
     $dbUsername = "root";
-    $dbPassword = "DDee@2022@DD";
-    $dbName     = "similie_db";
+    $dbPassword = "your_database_password";
+    $dbName     = "your_database_name";
 
     // Create database connection
     $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
